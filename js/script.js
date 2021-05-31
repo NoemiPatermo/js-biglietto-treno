098326bd4db5f78c34d5e142d35d982d8c0203b5
noemi86-cpu <noemipatermo@gmail.com>
2021-05-31 14:18:47 +0200

    esercizio

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 //chiedi quanti km vuoi percorrere
+//usala come costante 
+//var priceKm = 0.21
+
 
 var x = prompt('quanti km intendi percorrere?');
-var y = prompt('qual è la tua età?');
+var y = prompt('età del passeggero');
 
 //calcola prezzo totale del biglietto
 var prezzo = x * 0.21;
@@ -16,11 +19,21 @@ var prezzoOver = prezzo - scontoOver;
 
 //mostra il risultato(se sei minorenne se sei over e se sei nel range di else come età)
 if(y < 18 ){
-    document.getElementById('output').innerHTML = "Totale importo: " + prezzoMinorenni + " € ";
+    document.getElementById('output').innerHTML = "Totale importo: " + (prezzoMinorenni).toFixed(2) + " € ";
 }else{ if (y > 65){
-    document.getElementById('output').innerHTML = "Totale importo: " + prezzoOver + " € ";
+    document.getElementById('output').innerHTML = "Totale importo: " + (prezzoOver).toFixed(2) + " € ";
 }else{
-    document.getElementById('output').innerHTML = "Totale importo: " +  prezzo +  " € ";
+    document.getElementById('output').innerHTML = "Totale importo: " + (prezzo).toFixed(2) +  " € ";
 }
 
 }
+//oppure 
+//if (y < 18){
+    //var sconto = prezzo * scontoMinorenni/100;
+    //var result = prezzo - sconto;
+//} else if (y >= 65){
+    //var sconto = prezzo * scontoover/100;
+    //var result = prezzo - sconto;
+//} else {
+    //var result = prezzo;
+//}
